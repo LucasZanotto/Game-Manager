bb099e0ee9e0086a2344e1ad31f1c1fff766362b
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-10-27 10:55:56 -0300

    fix: resolvendo erros part 03

--- app/backend/src/middlewares/matchMiddle.ts
+++ app/backend/src/middlewares/matchMiddle.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export default function userMiddle(req: Request, res: Response, next: NextFunction) {
   const { homeTeam, awayTeam } = req.body;
   if (homeTeam > 16 || awayTeam > 16) {
-    res.status(404).json({ message: 'There is no team with such id!' });
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
   next();
 }
